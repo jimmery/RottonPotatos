6bc7914f451b786d4a9561b6371c3b03e0559641
Aaron Chung <Aaron Chung>
2016-10-19 16:37:15 -0700

    fixed order in which tables are dropped to work with foreign key constraints

--- reset.sql
+++ reset.sql
@@ -1,9 +1,8 @@
+DROP TABLE MovieActor;
+DROP TABLE MovieDirector;
+DROP TABLE Review;
+DROP TABLE MovieGenre;
 DROP TABLE Movie;
 DROP TABLE Actor;
-DROP TABLE Director;
-DROP TABLE MovieGenre;
-DROP TABLE MovieDirector;
-DROP TABLE MovieActor;
-DROP TABLE Review;
 DROP TABLE MaxPersonID;
 DROP TABLE MaxMovieID;
