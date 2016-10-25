ca29ec538c163f5c024aa3363da27aac856753b9
Jeffrey Jiang <jeffreyjiang@wifi-131-179-5-219.host.ucla.edu>
2016-10-25 16:25:16 -0700

    additional fixes

--- Project1C/search.php
+++ Project1C/search.php
@@ -9,8 +9,8 @@ if ($db->connect_errno > 0)
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
 
-echo "<h1>Actor Information Page: </h1>";
-echo "Search for an Actor:";
+echo "<h1>Searching Page: </h1>";
+echo "Search:";
 
 $search = "";
 $query = "";
@@ -22,74 +22,75 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 
 <form method="get" action="<?php echo
 htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <TEXTAREA NAME="search" ROWS=1 COLS=30></TEXTAREA> <br>
+    <input type="text" name="search"  value="">
     <input type="submit" name="submit" value="Click Me!">
 </form>
                                
 <?php
-
 echo "<br>";
-echo "<h2> Actors with this Name </h2>";
-/*$query = "SELECT * FROM Actor WHERE first = \"" . $search . "\"" .
-  " OR last = \"" . $search . "\";";*/
-$query = "SELECT * FROM Actor WHERE first LIKE '%" . $search . "%'" .
-       " OR last LIKE '%" . $search . "%';";
-echo $query . "<br>";
-$rs = $db->query($query);
 
-$attributes_defined = FALSE;
-echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"2\">";
-while ($row = $rs->fetch_assoc())
+
+if (strlen($search) != 0)
 {
-    if (!$attributes_defined)
+    $query = "SELECT * FROM Actor WHERE first LIKE '%" . $search . "%'" .
+           " OR last LIKE '%" . $search . "%';";
+
+    $rs = $db->query($query);
+    $attributes_defined = FALSE;
+    while ($row = $rs->fetch_assoc())
     {
+        if (!$attributes_defined)
+        {
+            echo "<h2> Actors with this Name </h2>";
+            echo $query . "<br>";
+            echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"2\">";
+            echo "<tr>";
+            echo "<th>Name</th>";
+            echo "<th>Date of Birth</th>";
+            echo "</tr>";
+            $attributes_defined = TRUE;
+        }
         echo "<tr>";
-        echo "<th>Name</th>";
-        echo "<th>Date of Birth</th>";
-        echo "</tr>";
-        $attributes_defined = TRUE;
-    }
-    echo "<tr>";
-    $name = $row["first"] . " " . $row["last"];
+        $name = $row["first"] . " " . $row["last"];
     $dob = $row["dob"];
     echo "<td>$name</td>";
     echo "<td>$dob</td>";
     
     echo "</tr>";
-}
-echo "</table>";
-
-print 'Total results: ' . $rs->num_rows. '<br>';
-$rs->free();
-
-echo "<h2> Movies who fit this search </h2>";
-$query = "SELECT * FROM Movie WHERE title LIKE '%" . $search . "%';";
-echo $query . "<br>";
-$rs = $db->query($query);
-
-$attributes_defined = FALSE;
-echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"2\">";
-while ($row = $rs->fetch_assoc())
-{
-    if (!$attributes_defined)
-    {
-        echo "<tr>";
-        echo "<th>Title</th>";
-        echo "<th>Release Year</th>";
-        echo "</tr>";
-        $attributes_defined = TRUE;
     }
-    echo "<tr>";
-    $title = $row["title"];
-    $year = $row["year"];
-    echo "<td>$title</td>";
-    echo "<td>$year</td>";
+    echo "</table>";
     
-    echo "</tr>";
+    //print 'Total results: ' . $rs->num_rows. '<br>';
+    $rs->free();
+    
+    $query = "SELECT * FROM Movie WHERE title LIKE '%" . $search . "%';";
+    $rs = $db->query($query);
+
+    $attributes_defined = FALSE;
+    while ($row = $rs->fetch_assoc())
+    {
+        if (!$attributes_defined)
+        {
+            echo "<h2> Movies who fit this search </h2>";
+            echo $query . "<br>";
+            echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"2\">";
+            echo "<tr>";
+            echo "<th>Title</th>";
+            echo "<th>Release Year</th>";
+            echo "</tr>";
+            $attributes_defined = TRUE;
+        }
+        echo "<tr>";
+        $title = $row["title"];
+        $year = $row["year"];
+        echo "<td>$title</td>";
+        echo "<td>$year</td>";
+        
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+    //print 'Total results: ' . $rs->num_rows;
+    $rs->free();
 }
-echo "</table>";
-
-print 'Total results: ' . $rs->num_rows;
-$rs->free();
-
 ?>
